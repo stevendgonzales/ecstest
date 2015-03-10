5466eb3d535f0a63d72042f81d00fb7fc13bc0fc
Steven Gonzales <stevendgonzales@gmail.com>
2015-03-10 18:04:27 -0500

    Create test scaffolding and first TestCase for user login.
    Add config module for loading settings from ENV vars.
    Create stub of restful test client for ECS Control Plane.
    Add custom testtools matcher for validating json against a schema.
    Create a custom TestBase class for TesCases that provides access to
    the test client and uses custom json matcher to implement
    self.assertJsonSchema(myschema, myjson)
    TestUserLogin TestCase that makes assertions on valid and invalid
    login attempts.

---
+++ ecstest/config.py
@@ -0,0 +1,21 @@
+"""Manage loading and return of test configuration settings."""
+
+from os import environ as env
+
+
+def get_config():
+    """Return a dictionary of configuration values."""
+
+    return {
+        'ADMIN_USERNAME': env.get('ADMIN_USERNAME', 'username'),
+        'ADMIN_PASSWORD': env.get('ADMIN_PASSWORD', 'password'),
+        'TOKEN': env.get('TOKEN', None),
+        'ECS_CONTROL_ENDPOINT':
+            env.get('ECS_CONTROL_ENDPOINT', 'https://127.0.0.1:4443'),
+        'ECS_TOKEN_ENDPOINT':
+            env.get('ECS_TOKEN_ENDPOINT', 'https://127.0.0.1:4443/login'),
+        'VERIFY_SSL': bool(int(env.get('VERIFY_SSL', 0))),
+        'REQUEST_TIMEOUT': float(env.get('REQUEST_TIMEOUT', 15.0)),
+        'TOKEN_FILENAME': env.get('TOKEN_FILENAME', '/tmp/ecstest.token'),
+        'CACHE_TOKEN': bool(int(env.get('CACHE_TOKEN', 1)))
+    }
---
+++ ecstest/constants.py
@@ -0,0 +1,5 @@
+"""Constants used throughout the test suite."""
+
+# http requests
+APPLICATION_JSON = 'application/json'
+AUTH_TOKEN_HEADER = 'x-sds-auth-token'
