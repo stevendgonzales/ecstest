555a69730487c544d79861c634dd7d1de4f784f2
lei.feng <flei2000@gmail.com>
2015-03-23 09:04:18 +0800

    Add verbose output mode.

--- ecstest/logger.py
+++ ecstest/logger.py
@@ -1,4 +1,4 @@
-import sys, logging
+import os, sys, logging
 
 def getLogger():
     '''Get a standard logger for testing'''
@@ -7,7 +7,11 @@ def getLogger():
 
     console_handler = logging.StreamHandler(sys.stdout)
     console_handler.setFormatter(logging.Formatter(formatter))
-    console_handler.setLevel(logging.INFO)
+    log_level = logging.INFO
+    verbose = os.environ.get('ECSTEST_VERBOSE_OUTPUT')
+    if verbose:
+        log_level = logging.DEBUG
+    console_handler.setLevel(log_level)
 
     logger = logging.getLogger("ecstest_logger")
     logger.setLevel(logging.DEBUG)
